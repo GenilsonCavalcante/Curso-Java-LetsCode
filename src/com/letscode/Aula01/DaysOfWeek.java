fea618922b77f0c6ca4efbf0e65dffcbb91b5723
GenilsonCavalcante <cavalcantedeoliveiragenilson@gmail.com>
2021-12-06 11:36:38 -0300

    First-Class

---
+++ src/com/letscode/Aula01/DaysOfWeek.java
@@ -0,0 +1,9 @@
+package com.letscode.Aula01;
+
+public enum DaysOfWeek {
+    Sunday, Monday, Tuesday, Wednesday, Thursday, Friday, Saturday;
+    /*
+    TRADUCTION:
+    Domingo, Segunda, Terça, Quarta, Quinta, Sexta, Sábado;
+    */
+}
---
+++ src/com/letscode/Aula01/MonthsOfTheYear.java
@@ -0,0 +1,5 @@
+package com.letscode.Aula01;
+
+public enum MonthsOfTheYear {
+        January, February, March, April, May, June, July, August, September, October, November, December;
+}
