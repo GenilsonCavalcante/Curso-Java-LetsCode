5473a45b38a384afa98d95aca8111ea0a5bdf81d
Genilson Cavalcante de Oliveira <cavalcantedeoliveiragenilson@gmail.com>
2021-12-06 11:48:38 -0300

    Delete src/com/letscode directory

--- src/com/letscode/Aula01/DaysOfWeek.java
+++
@@ -1,9 +0,0 @@
-package com.letscode.Aula01;
-
-public enum DaysOfWeek {
-    Sunday, Monday, Tuesday, Wednesday, Thursday, Friday, Saturday;
-    /*
-    TRADUCTION:
-    Domingo, Segunda, Terça, Quarta, Quinta, Sexta, Sábado;
-    */
-}
--- src/com/letscode/Aula01/MonthsOfTheYear.java
+++
@@ -1,5 +0,0 @@
-package com.letscode.Aula01;
-
-public enum MonthsOfTheYear {
-        January, February, March, April, May, June, July, August, September, October, November, December;
-}
