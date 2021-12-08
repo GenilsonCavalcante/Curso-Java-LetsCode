d7b9664156d7df42bd9a6d25d3f470dcfb413989
GenilsonCavalcante <cavalcantedeoliveiragenilson@gmail.com>
2021-12-08 09:25:31 -0300

    Class03 - Input & Output datas

---
+++ src/com/letscode/Class03/EntradasESaidas.java
@@ -0,0 +1,5 @@
+package com.letscode.Class03;
+
+public class EntradasESaidas {
+
+}
--- src/com/letscode/Main.java
+++ src/com/letscode/Main.java
@@ -1,40 +1,10 @@
 package com.letscode;
 
-import com.letscode.Class01.DaysOfWeek;
-import com.letscode.Class01.MonthsOfTheYear;
-import com.letscode.Class02.InputData;
-
-import java.awt.im.InputContext;
-
 public class Main {
 
     public static void main(String[] args) {
 
-        /*AULA 01
-        DaysOfWeek classDay = DaysOfWeek.Monday; //Object type variable
-
-        MonthsOfTheYear classMonth = MonthsOfTheYear.December;
-
-        System.out.println("Class on (" + classDay + ") of month (" + classMonth + ")");
-        */
-
-        //AULA 02
-        InputData teste = new InputData();
 
     }
 }
 
-/*
-enum MesesDoAno {
-    Janeiro (1), Fevereiro (2), Março (3);
-
-    public int mes;
-    MesesDoAno (int valor) {
-        mes = valor;
-    }
-}
-*********
-boolean verificacao = 10 == 10 & 10 == 20; //Existe uma diferença entre && e apenas um &. Com dois && verifica a primeira sentença, caso seja falsa, ela não irá verificar a segunda, retornando falso.
-System.out.println(verificacao);
-
-*/
